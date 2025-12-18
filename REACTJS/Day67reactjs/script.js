b22e6f27b05871a9ae85adc20ca27ea93389ac75
Arush Kumar <arushkumarbjn123@gmail.com>
2025-12-18 16:20:49 +0530

    add attribute instead of null parameter like as id, className etc

--- REACTJS/Day67reactjs/script.js
+++ REACTJS/Day67reactjs/script.js
@@ -1,6 +1,6 @@
-var h1 = React.createElement("h1", null, "Hello World")
-var h2 = React.createElement("h2", null, "Hello from h2")
-var p = React.createElement("p", null, "Hello from paragraph")
+var h1 = React.createElement("h1", {id:"heading1"}, "Hello World")
+var h2 = React.createElement("h2", {class:"heading2"}, "Hello from h2")
+var p = React.createElement("p", {id:"para", className:"para-class"}, "Hello from paragraph")
 
 var div = React.createElement("div", { id: "main" }, [h1, h2,p])
 
