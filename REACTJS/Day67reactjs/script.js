f043977b8b10f2352ee940c755c0f32d0eff4dfe
Arush Kumar <arushkumarbjn123@gmail.com>
2025-12-18 16:17:22 +0530

    if you create multiple elems

--- REACTJS/Day67reactjs/script.js
+++ REACTJS/Day67reactjs/script.js
@@ -1,5 +1,9 @@
 var h1 = React.createElement("h1", null, "Hello World")
+var h2 = React.createElement("h2", null, "Hello from h2")
+var p = React.createElement("p", null, "Hello from paragraph")
+
+var div = React.createElement("div", { id: "main" }, [h1, h2,p])
 
 var root = ReactDOM.createRoot(document.querySelector("#container"))
 
-root.render(h1)
+root.render(div)
