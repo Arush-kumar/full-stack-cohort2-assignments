fd517784a18287441428f5ba0c4eb287f6f6b238
Arush Kumar <arushkumarbjn123@gmail.com>
2025-12-20 16:32:53 +0530

    feat: initialize React project with Vite, add basic components and styling

---
+++ REACTJS/class1/src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav"
+import Footer from "./components/Footer"
+const App = () => {
+  return (
+    <>
+          <Nav />
+          <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ REACTJS/class1/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <h1>This is nav</h1>
+      </nav>
+    </>
+  )
+}
+
+export default Nav
