625115a5387b188fb0856efd12819d153352e612
Arush Kumar <arushkumarbjn123@gmail.com>
2025-12-23 12:41:33 +0530

    class3 props = obj

--- REACTJS/class3/src/components/Card.jsx
+++ REACTJS/class3/src/components/Card.jsx
@@ -1,14 +1,14 @@
 
-const Card = (props) => {
+const Card = (obj) => {
 
-  console.log(props);
+  console.log(obj);
   
 
   return (
     <div className="w-full bg-black card font-thin p-8 text-gray-500 text-5xl">
       <div className="h-fit w-fit p-8
       bg-amber-900"
-      >Hello, I am {props.user}, {props.age}</div>
+      >Hello, I am {obj.user}, {obj.age}</div>
     </div>
   )
 }
