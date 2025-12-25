41c828e93a41dc31ad4b88dcb1784a9a5ba684e7
Arush Kumar <arushkumarbjn123@gmail.com>
2025-12-25 17:39:34 +0530

    counter by useState update prev codebase

--- REACTJS/class5/src/App.jsx
+++ REACTJS/class5/src/App.jsx
@@ -2,18 +2,23 @@ import { useState } from "react"
 
 const App = () => {
 
-  const [user, setUser] = useState("Alen")
+  const [num, setNum] = useState(0)
 
-  const User = () => {
-    setUser("Alex")
-  }
   
   return (
-    <div className='h-screen bg-black text-white'>
-      <h1 className="text-5xl">{user}</h1>
-      <button onClick={User} 
-      className="bg-amber-600 px-4 py-2  w-fit">
-          Change User  
+    <div className='h-screen flex flex-col justify-center items-center gap-2 bg-black text-white p-8'>
+      <h1 className="text-8xl">{num}</h1>
+
+      <button onClick={() => setNum(num + 1)} 
+      className="rounded text-2xl bg-amber-600 px-4 py-2  w-fit"
+      >
+          Increment  
+      </button>
+
+      <button onClick={() => num > 0 ? setNum(num - 1):setNum(num)} 
+            className="rounded text-2xl bg-amber-600 px-4 py-2  w-fit"
+      >
+          Decrement  
       </button>
 
     </div>
