5985dc96dff481b0ab4c01b41f333c7183cc3766
Arush Kumar <arushkumarbjn123@gmail.com>
2025-12-25 17:55:46 +0530

    class5 a little bit updatation, many type of use case of useState

--- REACTJS/class5/src/App.jsx
+++ REACTJS/class5/src/App.jsx
@@ -2,23 +2,26 @@ import { useState } from "react"
 
 const App = () => {
 
+
+  const arr  = ["Hitesh", "Sarthak", "Harsh", "Piyush"]
+
   const [num, setNum] = useState(0)
 
   
   return (
-    <div className='h-screen flex flex-col justify-center items-center gap-2 bg-black text-white p-8'>
-      <h1 className="text-8xl">{num}</h1>
+    <div className='h-screen flex flex-col justify-center items-center gap-8 bg-black text-white p-8'>
+      <h1 className="text-8xl">{arr[num]}</h1>
 
-      <button onClick={() => setNum(num + 1)} 
+      <button onClick={() => {
+
+        if(num < arr.length -1){
+
+          setNum(num + 1)
+        }
+      }} 
       className="rounded text-2xl bg-amber-600 px-4 py-2  w-fit"
       >
-          Increment  
-      </button>
-
-      <button onClick={() => num > 0 ? setNum(num - 1):setNum(num)} 
-            className="rounded text-2xl bg-amber-600 px-4 py-2  w-fit"
-      >
-          Decrement  
+          Change User  
       </button>
 
     </div>
