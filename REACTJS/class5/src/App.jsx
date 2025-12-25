f62a9dc772cf40075c460306e1beb29c0874fbd4
Arush Kumar <arushkumarbjn123@gmail.com>
2025-12-25 21:18:41 +0530

    grace marks functionality and adjust rendering logic

--- REACTJS/class5/src/App.jsx
+++ REACTJS/class5/src/App.jsx
@@ -3,25 +3,31 @@ import { useState } from "react"
 const App = () => {
 
 
-  const arr  = ["Hitesh", "Sarthak", "Harsh", "Piyush"]
 
-  const [num, setNum] = useState(0)
+  const [marks, setMarks] = useState([28, 91, 85, 80, 27])
+
+  function graceStudent(){
+
+    const newMarks = marks.map((elem)=>{
+      if(elem>90) return elem
+      return elem + 5
+    })
+
+    setMarks(newMarks)
+  }
 
   
   return (
     <div className='h-screen flex flex-col justify-center items-center gap-8 bg-black text-white p-8'>
-      <h1 className="text-8xl">{arr[num]}</h1>
 
-      <button onClick={() => {
+      {marks.map(function(elem, idx) {
+        return <h1 key={idx}>Student {idx+1} = {elem} {elem>32?'PASS':'FAIL'}</h1>
+      })}
 
-        if(num < arr.length -1){
-
-          setNum(num + 1)
-        }
-      }} 
+      <button onClick={graceStudent}
       className="rounded text-2xl bg-amber-600 px-4 py-2  w-fit"
       >
-          Change User  
+          Give them grace  
       </button>
 
     </div>
