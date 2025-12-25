648805a73d10a90b5ca46ce961131c6285fbdd7e
Arush Kumar <arushkumarbjn123@gmail.com>
2025-12-25 12:28:01 +0530

    btn calling

--- REACTJS/class4/src/App.jsx
+++ REACTJS/class4/src/App.jsx
@@ -7,7 +7,11 @@ const App = () => {
   }
 
   return (
-    <div onClick={btnClick}
+    <div onClick={function()
+      {
+        btnClick();
+      }
+    }
       className='h-screen bg-black p-4'>
       <button className='bg-yellow-500 px-6 py-2 w-fit text-2xl rounded'>Click to Download</button>
     </div>
