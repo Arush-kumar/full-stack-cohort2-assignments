ed79074a14cd262779e55ee2a869e1f107d3a394
Arush Kumar <arushkumarbjn123@gmail.com>
2025-12-30 15:35:29 +0530

    class 7 final update

--- REACTJS/class7todos/src/App.jsx
+++ REACTJS/class7todos/src/App.jsx
@@ -1,97 +1,91 @@
-import { useState } from "react";
-import Card from "./components/Card";
+import { useState } from 'react'
+import Card from './components/Card'
 
 const App = () => {
-  const [userName, setUserName] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [userRole, setUserRole] = useState("");
-  const [userDescription, setUserDescription] = useState("");
 
-  const [allUsers, setAllUsers] = useState([]);
+  const [userName, setUserName] = useState('')
+  const [userRole, setUserRole] = useState('')
+  const [imageURL, setImageURL] = useState('')
+  const [userDesc, setUserDesc] = useState('')
+
+  const [allUsers, setAllUsers] = useState([])
 
   const submitHandler = (e) => {
-    e.preventDefault();
-    // console.log(userName, imageUrl, userRole, userDescription);
+    e.preventDefault()
 
-    // 2nd method
-    setAllUsers([
-      ...allUsers,
-      { userName, imageUrl, userRole, userDescription },
-    ]);
+    setAllUsers([...allUsers, { userName, userRole, userDesc, imageURL }])
 
-    setUserName("");
-    setImageUrl("");
-    setUserRole("");
-    setUserDescription("");
-  };
+    setUserName('')
+    setUserRole('')
+    setUserDesc('')
+    setImageURL('')
+  }
 
   const deleteHandler = (idx) => {
-    const copyUsers = [...allUsers];
-    copyUsers.splice(idx, 1);
-    // console.log(copyUsers);
+    const copyUsers = [...allUsers]
 
-    setAllUsers(copyUsers);
-  };
+    copyUsers.splice(idx, 1)
+
+    setAllUsers(copyUsers)
+  }
+
 
   return (
-    <div className="min-h-screen w-full bg-black text-gray-100 ">
-      <form
-        onSubmit={(e) => {
-          submitHandler(e);
-        }}
-        className="flex justify-center flex-wrap"
-      >
+    <div className='h-screen bg-black text-white'>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }} className='px-2 py-2 flex flex-wrap'>
+
         <input
           value={userName}
           onChange={(e) => {
-            setUserName(e.target.value);
+            setUserName(e.target.value)
           }}
-          className="text-xl font-semibold border-2 px-4 py-2 m-4 rounded w-[45%]"
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
           type="text"
-          placeholder="Enter your name"
-        />
+          placeholder='Enter your name' />
 
         <input
-          value={imageUrl}
+          value={imageURL}
           onChange={(e) => {
-            setImageUrl(e.target.value);
+            setImageURL(e.target.value)
           }}
-          className="text-xl font-semibold border-2 px-4 py-2 m-4 rounded w-[45%]"
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
           type="text"
-          placeholder="Image URL"
-        />
+          placeholder='Image URL' />
 
         <input
           value={userRole}
           onChange={(e) => {
-            setUserRole(e.target.value);
+            setUserRole(e.target.value)
           }}
-          className="text-xl font-semibold border-2 px-4 py-2 m-4 rounded w-[45%]"
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
           type="text"
-          placeholder="Enter Role"
-        />
+          placeholder='Enter Role' />
 
         <input
-          value={userDescription}
+          value={userDesc}
           onChange={(e) => {
-            setUserDescription(e.target.value);
+            setUserDesc(e.target.value)
           }}
-          className="text-xl font-semibold border-2 px-4 py-2 m-4 rounded w-[45%]"
+          className='border-2 text-xl font-semibold px-5 py-2 rounded m-2 lg:w-[48%]'
           type="text"
-          placeholder="Enter Description"
-        />
+          placeholder='Enter Description' />
 
-        <button className="text-xl font-semibold border-none px-10 py-2 bg-emerald-500 active:scale-97 transition-all cursor-pointer mt-4 rounded">
-          Create User
-        </button>
+
+        <button className=' px-5 py-2 text-xl active:scale-95 cursor-pointer font-semibold bg-emerald-700 rounded m-2 w-[96%]'>Create User</button>
       </form>
 
-      {/* Card Component Use */}
-      <div>
-        <Card elem={allUsers} deleteHandler={deleteHandler} />
+
+      <div className='px-4 py-10 gap-4 flex flex-wrap'>
+
+        {allUsers.map(function (elem, idx) {
+          return <Card key={idx} idx={idx} elem={elem} deleteHandler={deleteHandler} />
+        })}
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- REACTJS/class7todos/src/components/Card.jsx
+++ REACTJS/class7todos/src/components/Card.jsx
@@ -1,26 +1,22 @@
 
 const Card = (props) => {
-  // console.log(props);
-  
-  return (
-    <div className="px-4 py-4 flex flex-wrap">
-      {
-          props.elem.map((elem, idx)=>{
-            return (
-              <div key={idx} className="bg-gray-700 lg:bg-amber-300 md:bg-emerald-200 sm:bg-sky-200 m-4 p-6 rounded w-1/4 h-auto leading-tight">
-                <img src={elem.imageUrl} alt="" className="h-20 w-20 rounded-full mb-4 object-center object-cover"/>
-                <h2 className="text-3xl font-semibold mb-2">{elem.userName}</h2>
-                <h3 className="text-base text-blue-400 font-semibold ">{elem.userRole}</h3>
-                <p className="text-sm font-medium leading-tight text-wrap">{elem.userDescription}</p>
+    // console.log(props.elem);
+    
+    return (
+        <div className='lg:w-[23vw] md:w-[30vw] sm:w-[45vw] rounded-xl py-8 px-8 flex items-center flex-col text-center bg-white text-black'>
+          {/* Profile Pic */}
+            <img className='h-24 w-24 rounded-full object-center object-cover' src={props.elem.imageURL} alt="" />
+            <h1 className='text-2xl mt-2 font-bold'>{props.elem.userName}</h1>
+            {/* Job Role */}
+            <h5 className=' text-blue-500 text-lg font-semibold my-2'>{props.elem.userRole}</h5>
+            {/* User Description */}
+            <p className='text-sm font-medium leading-tight'>{props.elem.userDesc}</p>
 
-                <button onClick={()=>props.deleteHandler(idx)}
-                className="px-4 py-2 rounded text-xs font-bold bg-red-500 mt-6 active:scale-95 cursor-pointer">Remove</button>
-              </div>
-            )
-          })
-        }
-    </div>
-  )
+            <button onClick={() => {
+                props.deleteHandler(props.idx)
+            }} className='px-4 py-2 rounded text-xs cursor-pointer active:scale-95 bg-red-600 text-white font-semibold mt-3'>Remove</button>
+        </div>
+    )
 }
 
 export default Card
