46eb7b633f1e330d952e10c305868461facfa96f
Arush Kumar <arushkumarbjn123@gmail.com>
2026-01-01 12:24:34 +0530

    Day77 class9, axios and useEffect hook

--- REACTJS/class9/src/App.jsx
+++ REACTJS/class9/src/App.jsx
@@ -1,17 +1,34 @@
 import axios from "axios"
+import { useEffect, useState } from "react"
+import UserCard from "./components/UserCard"
 const App = () => {
 
+  const [users, setUsers] = useState([])
   const getData = async()=> {
     
     const response = await axios.get("https://jsonplaceholder.typicode.com/users")
-    console.log(response.data[1].name)
+    console.log(response.data)
     
+    setUsers(response.data)
   }
 
+  useEffect(() => {
+    getData()
+  }, [])
+
+
+
   return (
-    <>
-      <button onClick={getData} className="btn">Click</button>
-    </>
+    <div>
+      
+      <div className='all-cards'>
+        {users.map(function (elem, idx) {
+          return <div key={idx}>
+            <UserCard elem={elem} />
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 
