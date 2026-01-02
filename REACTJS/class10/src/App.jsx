b2d8436ec17ca3d582b703377e2ce23981c10c40
Arush Kumar <arushkumarbjn123@gmail.com>
2026-01-02 11:45:02 +0530

    useEffect concept get data from pokemon API

--- REACTJS/class10/src/App.jsx
+++ REACTJS/class10/src/App.jsx
@@ -1,20 +1,27 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
-
 const App = () => {
 
+  const [allPokemons, setAllPokemons] = useState([])
   const getData = async () => {
     const response = await axios.get("https://pokeapi.co/api/v2/pokemon/")
-    console.log(response.data.results[0].name);
-
+    // console.log(response.data.results[0].name);
+    setAllPokemons(response.data.results)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getData()
-  },[])
+  }, [])
+  
   return (
-    <div>
-      
+    <div className="h-screen bg-black text-white text-3xl">
+      {/* <button onClick={getData}>click</button> */}
+
+      {
+        allPokemons.map((elem) => {
+          return <h1>{elem.name}</h1>
+        })
+      }
     </div>
   )
 }
