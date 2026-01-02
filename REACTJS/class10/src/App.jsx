f17be69d0d515769fa771e893ce4da04aec2fda7
Arush Kumar <arushkumarbjn123@gmail.com>
2026-01-02 12:22:58 +0530

    useEffect concept get data from random  API for random user showcase

--- REACTJS/class10/src/App.jsx
+++ REACTJS/class10/src/App.jsx
@@ -2,26 +2,25 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 const App = () => {
 
-  const [allPokemons, setAllPokemons] = useState([])
+  const [user, setUser] = useState('')
+  const [num, setNum] = useState(0)
   const getData = async () => {
-    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/")
-    // console.log(response.data.results[0].name);
-    setAllPokemons(response.data.results)
+    const response = await axios.get("https://randomuser.me/api/")
+    setUser((response.data.results[0].name.first)+" "+(response.data.results[0].name.last))
   }
 
   useEffect(() => {
     getData()
-  }, [])
+  }, [num])
   
-  return (
-    <div className="h-screen bg-black text-white text-3xl">
-      {/* <button onClick={getData}>click</button> */}
 
-      {
-        allPokemons.map((elem) => {
-          return <h1>{elem.name}</h1>
-        })
-      }
+
+  return (
+    <div className="h-[15vw] w-1/3 bg-black text-white text-3xl text-center pt-4">
+      {user}
+      {/* <h1>{num}</h1> */}
+      <br /> <br />
+      <button id='btn' onClick={() => setNum(num+1)}>click here</button>
     </div>
   )
 }
