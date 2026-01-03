f3a1988b254601757c6099f0de1da583c3a8238a
Arush Kumar <arushkumarbjn123@gmail.com>
2026-01-04 01:35:08 +0530

    value of this

---
+++ Javascript/AdvanceJs/56 Advance js1/script1.js
@@ -0,0 +1,56 @@
+// console.log(this);
+//Es5 function
+// function fun(){
+//   console.log(this);
+// }
+// fun()
+
+//Es5 func inside object
+// let obj = {
+//   name: "Arush",
+//   age: 20,
+//   fun: function () {
+//     console.log(this);
+//   },
+// }
+
+// obj.fun();
+
+
+// Es6 func inside object
+// let obj = {
+//   name: "Arush",
+//   age: 20,
+//   fun: () =>{
+//     console.log(this);
+//   }
+// }
+// obj.fun()
+
+// Es5 inside Es5 inside object
+// let obj = {
+//   name: "Arush",
+//   age: 20,
+//   fun: function () {
+//     function fun1() {
+//       console.log(this);
+//     }
+//     fun1();
+//   },
+// }
+
+// obj.fun();
+
+// Es6 inside Es5 inside object
+let obj = {
+  name: "Arush",
+  age: 20,
+  fun: function () {
+    fun1 = () => {
+      console.log(this);
+    }
+    fun1();
+  },
+}
+
+obj.fun();
