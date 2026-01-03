50a64bc4c6b13d094e48992f0042652167bc9804
Arush Kumar <arushkumarbjn123@gmail.com>
2026-01-03 12:31:51 +0530

    Day 57 JS2 assignment

---
+++ Javascript/AdvanceJs/57 Advance js2/script.js
@@ -0,0 +1,12 @@
+class Sketch{
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+    this.someFunc = function(){}
+  }
+  speak(){}
+  walk(){}
+}
+
+let ch1 = new Sketch('Doremon', 'Blue');
+let ch2 = new Sketch("Doreni", "Yellow");
