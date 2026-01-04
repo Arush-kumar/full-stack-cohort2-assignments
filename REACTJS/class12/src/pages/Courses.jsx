19e7019a7881073f06e32ec517d0abdb707dc493
Arush Kumar <arushkumarbjn123@gmail.com>
2026-01-04 14:33:29 +0530

    routing and basic components & nested route

---
+++ REACTJS/class12/src/pages/Courses.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom"
+
+const Courses = () => {
+  return (
+    <div>
+      <div className="sale">
+        <p>sale is live</p>
+        <p>sale is live</p>
+        <p>sale is live</p>
+        <p>sale is live</p>
+        <p>sale is live</p>
+        <p>sale is live</p>
+        <p>sale is live</p>
+        <p>sale is live</p>
+        <p>sale is live</p>
+        <p>sale is live</p>
+      </div>
+
+      <Outlet />
+    </div>
+  )
+}
+
+export default Courses
---
+++ REACTJS/class12/src/pages/Kodex.jsx
@@ -0,0 +1,10 @@
+
+const Kodex = () => {
+  return (
+    <div>
+      <h2>Kodex Course</h2>
+    </div>
+  )
+}
+
+export default Kodex
