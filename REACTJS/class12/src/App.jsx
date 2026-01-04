868941115d427daa003e229db81409ee33aa4513
Arush Kumar <arushkumarbjn123@gmail.com>
2026-01-04 15:13:11 +0530

    add new components Layout, routuing perform with createBrowserRoute

--- REACTJS/class12/src/App.jsx
+++ REACTJS/class12/src/App.jsx
@@ -1,31 +1,60 @@
-import { Route, Routes } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Courses from "./pages/Courses"
-import AllCourses from "./pages/AllCourses"
-import Kodr from "./pages/Kodr"
-import Kodex from "./pages/Kodex"
-import Footer from "./components/Footer"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Courses from "./pages/Courses";
+import AllCourses from "./pages/AllCourses";
+import Kodr from "./pages/Kodr";
+import Kodex from "./pages/Kodex";
+import Layout from "./pages/Layout";
 
 const App = () => {
+  {
+    /* 2st Method to create a Routes */
+  }
+  const allRoutes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "courses",
+          element: <Courses />,
+          children: [
+            {
+              index: true,
+              element: <AllCourses />,
+            },
+            {
+              path: "kodr",
+              element: <Kodr />,
+            },
+            {
+              path: "kodex",
+              element: <Kodex />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/courses" element={<Courses />} >
-          <Route path="/courses/" element={<AllCourses />} />
-          <Route path="/courses/kodr" element={<Kodr />} />
-          <Route path="/courses/kodex" element={<Kodex />} />
-        </Route>
-      </Routes>
+      <RouterProvider router={allRoutes} />
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
