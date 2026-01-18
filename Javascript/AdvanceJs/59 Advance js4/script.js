988e06c2cf073a1eec2f733316b3552240b775c7
Arush Kumar <arushkumarbjn123@gmail.com>
2026-01-18 13:10:02 +0530

    Day 59 callback hell

---
+++ Javascript/AdvanceJs/59 Advance js4/script.js
@@ -0,0 +1,19 @@
+// callback hell
+function abcd(fn) {
+  fn(function (fn2) {
+    fn2(function (fn4) {
+      fn4(function () {
+        console.log("Hey");
+        
+      });
+    });
+  });
+}
+
+abcd(function (fn1) {
+  fn1(function (fn3) {
+    fn3(function (fn5) {
+      fn5();
+    });
+  });
+});
